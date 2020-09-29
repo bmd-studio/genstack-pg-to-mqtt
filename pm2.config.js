d10f6f8388b88b53c0333db0149bc3b7f702ff53
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2020-09-29 09:33:55 +0200

    chore: simplified the way the development and production environment are run in the container.

---
+++ pm2.config.js
@@ -0,0 +1,30 @@
+const {
+  SERVICE_NAME,
+  PM2_WATCH_DELAY,
+  PM2_MAX_RESTARTS,
+  DEFAULT_DEBUG_PORT,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: `${SERVICE_NAME}-build`,
+      script: 'yarn',
+      args: ['build:watch'],
+      interpreter: '/bin/sh',
+    },        
+    {
+      name: SERVICE_NAME,
+      script: 'dist/index.js',
+      watch: true,
+      watch_delay: PM2_WATCH_DELAY,
+      max_restarts: PM2_MAX_RESTARTS,
+      ignore_watch: ['node_modules'],
+      watch_options: {
+        usePolling: true,
+      },              
+      interpreter: '/usr/local/bin/node',
+      interpreter_args: `--inspect=0.0.0.0:${DEFAULT_DEBUG_PORT}`,
+    }
+  ]
+};
--- setup.sh
+++ setup.sh
@@ -1,17 +1,8 @@
-#!/bin/bash -x
+#!/bin/sh -x
 
 if [ "$GS_ENV" == "development" ]; then
-    echo "Setting up development mode..."
-    if ! [ -x "$(command -v npm)" ]; then
-        echo "Installing Node..."
-        curl -sL https://deb.nodesource.com/setup_12.x | bash -
-        apt-get install -y nodejs
-    fi
-    echo "Installing PM2..."
-    npm install pm2@4.4.0 -g
+    echo "Setting up development..."
+else
+    echo "Setting up production..."
+    npm prune --production
 fi
-
-yarn
-yarn build
-
-
