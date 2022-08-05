c8bea6c3caf6a124cc2b94279c761132d6dad891
Pepijn Verburg <pepijn@moeilijkedingen.nl>
2022-08-05 15:03:57 +0200

    refactor: the NPM module will now not automatically start the process, but only export the process handlers. There is now a dedicated entry-point for starting the process directly to be used in the PM2 and Docker configurations.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,15 @@
-import { startProcess, stopProcess } from './process';
-
 export interface PgEvent {
   processId: string;
   payload: RowOperationPayload;
 }
 
 export interface RowOperationPayload {
-  operation: string; 
-  tableName: string; 
-  rowId: string; 
+  operation: string;
+  tableName: string;
+  rowId: string;
   columnName: string;
   columnValue: string;
   notifyId: string;
 }
 
-process.on('SIGINT', () => {
-  stopProcess();
-});
-
-(async(): Promise<void> => {
-  startProcess();
-})();
-
-
+export { startProcess, stopProcess } from './process';
---
+++ src/start.ts
@@ -0,0 +1,21 @@
+import logger from './logger';
+import { startProcess, stopProcess } from './process';
+
+logger.info(`Starting the main process...`);
+
+process.on('SIGINT', () => {
+  stopProcess();
+});
+
+(async(): Promise<void> => {
+	try {
+		await startProcess();
+		logger.info(`🚀 Ready to bridge PG to MQTT events.`);
+	} catch (error) {
+		logger.error(`An error occurred during startup:`);
+		logger.error(error);
+		process.exit(1);
+	}
+})();
+
+
